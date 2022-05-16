337341e3670647afb18389c3b3007d7cfbb18876
Manuel Riezebosch <manuel.riezebosch@nl.abnamro.com>
2022-05-16 11:44:33 +0200

    fix(function): stop on error

--- function/azcopy/run.ps1
+++ function/azcopy/run.ps1
@@ -1,19 +1,17 @@
 # Input bindings are passed in via param block.
 param($Timer)
-
-# Get the current universal time in the default string format.
-$currentUTCtime = (Get-Date).ToUniversalTime()
+$ErrorActionPreference = "Stop"
 
 # The 'IsPastDue' property is 'true' when the current function invocation is later than scheduled.
 if ($Timer.IsPastDue) {
     Write-Host "PowerShell timer is running late!"
 }
 
-# Write an information log with the current time.
-Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
-
-$env:AZCOPY_LOG_LOCATION = $env:AZCOPY_JOB_PLAN_LOCATION = "$env:TEMP\.azcopy"
+$env:AZCOPY_LOG_LOCATION = $env:AZCOPY_JOB_PLAN_LOCATION = "$env:TEMP\azcopy"
 $env:AZCOPY_CONCURRENCY_VALUE = 1000
 $env:AZCOPY_CONCURRENT_SCAN = 1000
 
 & $PSScriptRoot\azcopy.exe sync $env:SOURCE $env:SINK --delete-destination true | Write-Host
+if ($LASTEXITCODE -ne 0) {
+    throw "Exit code: $LASTEXITCODE"
+}
